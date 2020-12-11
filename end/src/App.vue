2f35d9a853c0dbc8211b82ea5ed33bc713a934e4
Bibek Shakya <bibekneva@gmail.com>
2020-12-11 16:50:52 +0000

    files and folders added

---
+++ end/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Repository />
+  </div>
+</template>
+
+<script>
+import Repository from './components/Repository'
+
+export default {
+  name: 'App',
+  components: {
+    Repository
+  }
+}
+</script>
+
---
+++ end/src/components/Repository.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="mb-6">
+    <section class="hero is-medium is-primary is-bold mb-6">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Get GitHub Repositories</h1>
+          <h2 class="subtitle">using Vue JS, REST API &amp; Bulma</h2>
+        </div>
+      </div>
+    </section>
+    <div class="container">
+      <table class="table is-bordered is-striped is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>URL</th>
+            <th>Language</th>
+            <th>Login</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="repo in repos" v-bind:key="repo.id">
+            <td>{{ repo.id }}</td>
+            <td>{{ repo.name }}</td>
+            <td>{{ repo.html_url }}</td>
+            <td>{{ repo.language }}</td>
+            <td>{{ repo.owner.login }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Repository",
+  data() {
+    return {
+      repos: null,
+    };
+  },
+  created: function () {
+    axios.get("https://api.github.com/users/bibeva/repos").then((response) => {
+      this.repos = response.data;
+    });
+  },
+};
+</script>
+
+<style>
+body {
+  font: 15px/1.8 "Poppins", sans-serif !important;
+}
+
+.table td,
+.table th {
+  padding: 20px !important;
+}
+</style>
